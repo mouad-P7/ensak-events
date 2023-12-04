9027f0032fe04ea6c9978eff05220e42a58ac8fe
mouad-p7 <mouadananouch7@gmail.com>
2023-12-04 17:24:28 +0100

    Update footer.php.

--- utils/footer.php
+++ utils/footer.php
@@ -5,32 +5,21 @@
             <section>
                 <div class = "footerContent col-md-4"><!--left content-->
                     <p class = "footerContent1">
-                        <strong>I</strong><span class = "small footerSubtext">nstitute of</span>
-                        <strong>A</strong><span class = "small footerSubtext">rt</span>
-                        <strong>D</strong><span class = "small footerSubtext">esign and</span>
-                        <strong>T</strong><span class = "small footerSubtext">echnology</span>
+                        <strong>E</strong><span class = "small footerSubtext">cole</span>
+                        <strong>N</strong><span class = "small footerSubtext">ationale des </span>
+                        <strong>S</strong><span class = "small footerSubtext">ciences</span>
+                        <strong>A</strong><span class = "small footerSubtext">ppliquées</span>
                     </p>
 
                     <p class = "footerSubtext2">
-                        Kill Avenue, Dun Laoghaire, Co. Dublin
-                        &copy; Urban Events Venue & Catering, 2016.
+                    Adresse : campus universitaire, B.P 241, Kénitra - Maroc
                     </p>
                 </div>
             </section>
             <section>
                 <div class = "footcontent col-md-4"><!--middle content-->
-                    DL824 Multimedia Design/Web Engineering<br>
-                    Creative Computing 2015-2016.<br>
-                    Julz Engracio, N00145647
-                </div>
-            </section>
-            <section>
-                <div class = "footcontent col-md-4"><!--right content-->
-                    Follow Us:<br>
-                        <img src = "images/facebook.png">
-                        <img src = "images/twitter.png">
-                        <img src = "images/googleplus.png">
-                        <img src = "images/youtube.png">
+                    Ecole Nationale des Sciences Appliquées <br>
+                    Tél : (+212) 5 37 32 94 48/(+212) 5 37 37 67 65 | Fax : (+212) 5 37 37 40 52
                 </div>
             </section>
         </div>
